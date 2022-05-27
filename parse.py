dfcc143435f0be4e240155bddc276f974e82ad83
yk <yefim_korshever@mail.ru>
2022-05-27 19:50:40 +0300

    Get and save dvmn.svg

---
+++ parse.py
@@ -0,0 +1,11 @@
+import requests
+
+
+url = "https://dvmn.org/filer/canonical/1542890876/16/"
+
+response = requests.get(url)
+response.raise_for_status()
+
+filename = 'tmp/dvmn.svg'
+with open(filename, 'wb') as file:
+    file.write(response.content)
