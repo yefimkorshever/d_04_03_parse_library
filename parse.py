45aa556be89b7ca4455648b50d365b70b4c7b1cf
yk <yefim_korshever@mail.ru>
2022-05-27 20:15:46 +0300

    Save one book

--- parse.py
+++ parse.py
@@ -1,11 +1,11 @@
 import requests
 
 
-url = "https://dvmn.org/filer/canonical/1542890876/16/"
+url = "https://tululu.org/txt.php?id=32168"
 
 response = requests.get(url)
 response.raise_for_status()
 
-filename = 'tmp/dvmn.svg'
-with open(filename, 'wb') as file:
-    file.write(response.content)
+filename = 'tmp/sands_of_mars.txt'
+with open(filename, 'w', encoding='utf-8') as file:
+    file.write(response.text)
