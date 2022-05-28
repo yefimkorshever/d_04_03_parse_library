bc897418888e511a0b71e227009de6b502c92efd
yk <yefim_korshever@mail.ru>
2022-05-28 11:06:30 +0300

    download books from tululu.org

--- parse.py
+++ parse.py
@@ -1,11 +1,24 @@
+from pathlib import Path
+
 import requests
 
 
-url = "https://tululu.org/txt.php?id=32168"
+def download_books():
+    catalog_name = 'books'
+    Path(f'./{catalog_name}').mkdir(exist_ok=True)
+    for counter in range(1, 11):
+        url = f"https://tululu.org/txt.php?id={counter}"
+        response = requests.get(url)
+        response.raise_for_status()
 
-response = requests.get(url)
-response.raise_for_status()
+        filename = f'{catalog_name}/id{counter}.txt'
+        with open(filename, 'w', encoding='utf-8') as file:
+            file.write(response.text)
 
-filename = 'tmp/sands_of_mars.txt'
-with open(filename, 'w', encoding='utf-8') as file:
-    file.write(response.text)
+
+def main():
+    download_books()
+
+
+if __name__ == '__main__':
+    main()
