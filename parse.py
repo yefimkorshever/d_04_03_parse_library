3403484e21e3cda60bbf24694b03ec0e824c760d
yk <yefim_korshever@mail.ru>
2022-05-28 14:48:51 +0300

    Check redirect

--- parse.py
+++ parse.py
@@ -3,15 +3,26 @@ from pathlib import Path
 import requests
 
 
+def check_for_redirect(response):
+    if response.url == 'https://tululu.org/' and response.history:
+        raise requests.HTTPError()
+
+
 def download_books():
     catalog_name = 'books'
     Path(f'./{catalog_name}').mkdir(exist_ok=True)
-    for counter in range(1, 11):
-        url = f"https://tululu.org/txt.php?id={counter}"
+
+    for book_id in range(1, 11):
+        url = f"https://tululu.org/txt.php?id={book_id}"
         response = requests.get(url)
         response.raise_for_status()
 
-        filename = f'{catalog_name}/id{counter}.txt'
+        try:
+            check_for_redirect(response)
+        except requests.RequestException:
+            continue
+
+        filename = f'{catalog_name}/id{book_id}.txt'
         with open(filename, 'w', encoding='utf-8') as file:
             file.write(response.text)
 
