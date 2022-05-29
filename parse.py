c87a845ea886c5a03651d13e9eda3b52eb442f7c
yk <yefim_korshever@mail.ru>
2022-05-29 16:15:09 +0300

    download books with titles

--- parse.py
+++ parse.py
@@ -11,64 +11,58 @@ def check_for_redirect(response):
         raise requests.HTTPError()
 
 
+def get_response(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    return response
+
+
+def parse_book_page(text):
+    soup = BeautifulSoup(text, 'lxml')
+    title_tag = soup.find('table').find('h1')
+    title_text = title_tag.text
+    book_name = title_text.split(sep='::')
+
+    return {
+        'title': book_name[0].strip(),
+        'author': book_name[1].strip(),
+    }
+
+
+def download_txt(url, filename, folder):
+    response = get_response(url)
+    try:
+        check_for_redirect(response)
+    except requests.RequestException:
+        return
+
+    valid_filename = f'{sanitize_filename(filename)}.txt'
+    file_path = os.path.join(folder, valid_filename)
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(response.text)
+
+
 def download_books():
-    catalog_name = 'books'
-    Path(f'./{catalog_name}').mkdir(exist_ok=True)
+    folder = 'books'
+    Path(f'./{folder}').mkdir(exist_ok=True)
 
     for book_id in range(1, 11):
-        url = f"https://tululu.org/txt.php?id={book_id}"
-        response = requests.get(url)
-        response.raise_for_status()
-
+        head_url = 'https://tululu.org/'
+        book_page_response = get_response(f'{head_url}b{book_id}')
         try:
-            check_for_redirect(response)
+            check_for_redirect(book_page_response)
         except requests.RequestException:
             continue
 
-        filename = f'{catalog_name}/id{book_id}.txt'
-        with open(filename, 'w', encoding='utf-8') as file:
-            file.write(response.text)
-
-
-def get_name_and_author():
-    url = 'https://tululu.org/b1/'
-    response = requests.get(url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
-    title_tag = soup.find('table').find('h1')
-    title_text = title_tag.text
-    book_card = title_text.split(sep='::')
-    title = book_card[0].strip()
-    author = book_card[1].strip()
-
-    print(f'Title: {title}', f'Author: {author}', sep='\n')
-
-
-def download_txt(url, filename, folder='books'):
-    """Функция для скачивания текстовых файлов.
-    Args:
-        url (str): Cсылка на текст, который хочется скачать.
-        filename (str): Имя файла, с которым сохранять.
-        folder (str): Папка, куда сохранять.
-    Returns:
-        str: Путь до файла, куда сохранён текст.
-    """
-    valid_filename = f'{sanitize_filename(filename)}.txt'
-    return os.path.join(folder, valid_filename)
+        book_card = parse_book_page(book_page_response.text)
+        title = book_card['title']
+        download_txt(
+            f'{head_url}txt.php?id={book_id}',
+            f'{book_id}.{title}',
+            folder)
 
 
 def main():
-    url = 'http://tululu.org/txt.php?id=1'
-    filepath = download_txt(url, 'Алиби')
-    print(filepath)  # Выведется books/Алиби.txt
-    filepath = download_txt(url, 'Али/би', folder='books')
-    print(filepath)  # Выведется books/Алиби.txt
-
-    filepath = download_txt(url, 'Али\\би', folder='txt')
-    print(filepath)  # Выведется txt/Алиби.txt
-
-    return
-    get_name_and_author()
     download_books()
 
 
