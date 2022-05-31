5768df649eb6fb57c436f9b5ded9c9e77cb919cb
yk <yefim_korshever@mail.ru>
2022-05-31 16:38:23 +0300

    download book covers

--- parse.py
+++ parse.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from urllib.parse import unquote, urljoin, urlsplit
 
 import requests
 from bs4 import BeautifulSoup
@@ -17,19 +18,39 @@ def get_response(url):
     return response
 
 
-def parse_book_page(text):
-    soup = BeautifulSoup(text, 'lxml')
-    title_tag = soup.find('table').find('h1')
-    title_text = title_tag.text
+def parse_book_page(response):
+    soup = BeautifulSoup(response.text, 'lxml')
+    div_content = soup.find('div', id="content")
+    title_text = div_content.find('h1').text
     book_name = title_text.split(sep='::')
+    img_src = div_content.find('img')['src']
 
     return {
         'title': book_name[0].strip(),
         'author': book_name[1].strip(),
+        'image': urljoin(response.url, img_src),
     }
 
 
-def download_txt(url, filename, folder):
+def download_image(url):
+    folder = 'images'
+    Path(f'./{folder}').mkdir(exist_ok=True)
+    response = get_response(url)
+    try:
+        check_for_redirect(response)
+    except requests.RequestException:
+        return
+
+    filename = urlsplit(url).path.split(sep='/')[-1]
+    valid_filename = unquote(filename)
+    file_path = os.path.join(folder, valid_filename)
+    with open(file_path, 'wb') as file:
+        file.write(response.content)
+
+
+def download_txt(url, filename):
+    folder = 'books'
+    Path(f'./{folder}').mkdir(exist_ok=True)
     response = get_response(url)
     try:
         check_for_redirect(response)
@@ -43,8 +64,6 @@ def download_txt(url, filename, folder):
 
 
 def download_books():
-    folder = 'books'
-    Path(f'./{folder}').mkdir(exist_ok=True)
 
     for book_id in range(1, 11):
         head_url = 'https://tululu.org/'
@@ -54,12 +73,14 @@ def download_books():
         except requests.RequestException:
             continue
 
-        book_card = parse_book_page(book_page_response.text)
+        book_card = parse_book_page(book_page_response)
+        download_image(book_card['image'])
+
         title = book_card['title']
         download_txt(
             f'{head_url}txt.php?id={book_id}',
             f'{book_id}.{title}',
-            folder)
+        )
 
 
 def main():
